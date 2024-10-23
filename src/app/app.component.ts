76b2d05186913f9fe4a82d13163fff4e3b064511
Uroš Ostojić <urosostojic97@gmail.com>
2024-10-24 00:49:44 +0200

    Fix styles on mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ExercisesComponent } from './components/exercises/exercises.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -8,6 +8,7 @@ import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { Auth, onAuthStateChanged } from '@angular/fire/auth';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   encapsulation: ViewEncapsulation.None,
@@ -22,20 +23,23 @@ import { CommonModule } from '@angular/common';
     WorkoutsComponent, 
     CalendarComponent, 
     NavbarComponent,
-    CommonModule ],
+    CommonModule 
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'fit-me';
   isAuthenticated = false;
+  isLoading = true; // Add loading state
 
   constructor(private auth: Auth, private router: Router) {
     // Listen for authentication state changes
     onAuthStateChanged(this.auth, (user) => {
       this.isAuthenticated = !!user;
-      
-      // Only redirect if user is authenticated and not already on login or register pages
+      this.isLoading = false; // Set loading to false once state is determined
+
+      // Redirect if user is authenticated and on login or register pages
       if (this.isAuthenticated && (this.router.url === '/login' || this.router.url === '/register')) {
         this.router.navigate(['/dashboard']);
       }
